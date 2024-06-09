6f4f264cfec7f9f443358f820dc43b0efc87ec5d
Daviesss <ogunsdavis53@gmail.com>
2024-06-09 16:37:39 +0100

    Inheritance script from the main script

--- lidar_ros_wrapper/src/scripts/YdlidarX2_ros_wrapper.py
+++ lidar_ros_wrapper/src/scripts/YdlidarX2_ros_wrapper.py
@@ -1,7 +1,7 @@
 from  include import yd_lidarX2
 
-pub_scan_data = yd_lidarX2.Ydlidar_ros_wrapper()
+scan_data = yd_lidarX2.Ydlidar_ros_wrapper()
 
 if __name__ == '__main__':
-    pub_scan_data.scan_data()
+    scan_data.scan_data()
     
