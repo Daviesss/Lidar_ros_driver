d6f51ec8501c1e6343cb1cbf7f66257e16d4771b
Daviesss <ogunsdavis53@gmail.com>
2024-06-09 16:39:48 +0100

    Main implementation script

--- lidar_ros_wrapper/src/scripts/include/yd_lidarX2.py
+++ lidar_ros_wrapper/src/scripts/include/yd_lidarX2.py
@@ -1,86 +1,124 @@
 #!/usr/bin/env python3
-'''
-  Node implementation of YDLidar X2 ros wrapper,
-  contains library , class ,methods 
-  and also implementation of the ROS 1 wrappper for YDlidarX2
-'''
-# import Libraries..
+"""
+  Node implementation of YDLidar X2 ROS wrapper,
+  contains library, class, methods
+  and also implementation of the ROS 1 wrapper for YDLidarX2.
+
+  Author: Davies Iyanuoluwa Ogunsina 
+  
+  MIT License
+
+  Copyright (c) [2024] Davies Iyanuoluwa Ogunsina
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+"""
 import rospy
-import ydlidar 
+import ydlidar
 from sensor_msgs.msg import LaserScan
+import math, random
 
-
-# implementation class....
+# Implementation class
 class Ydlidar_ros_wrapper:
     def __init__(self):
-        rospy.init_node('lidar_ros_wrapper',anonymous=True)
-        self.laser_scan_data = LaserScan() # store laser scan data type 
-        self.laser_pub = rospy.Publisher("/scan",LaserScan,queue_size=10) # Publish over to a topic name called "/scan"
-        self.port = rospy.get_param('~port',"/dev/ttyUSB0")
-        self.buad_rate = rospy.get_param('~baudrate',115200)
-        self.scan_frequency = rospy.get_param('~scan_frequency',10.0)
-        self.sample_rate = rospy.get_param('~sampleRate',9)
-        self.singlechannel = rospy.get_param('~SingleChannel',True) # Set to True for pub laser scan.....
-        self.max_range = rospy.get_param('~max_range',12.0)
-        # self.min_range = rospy.get_param('~min_range',6.0)
+        rospy.init_node('lidar_ros_wrapper', anonymous=True)
+        self.laser_pub = rospy.Publisher("/scan", LaserScan, queue_size=10)  # Publish to the topic named "/scan"
+        self.port = rospy.get_param('~port', "/dev/ttyUSB4")
+        self.baud_rate = rospy.get_param('~baudrate', 115200)
+        self.scan_frequency = rospy.get_param('~scan_frequency', 10.0)
+        self.sample_rate = rospy.get_param('~sampleRate', 9)
+        self.singlechannel = rospy.get_param('~SingleChannel', True)  # Set to True for pub laser scan
+        self.max_range = rospy.get_param('~max_range', 12.0)
         self.lidar_type = rospy.get_param('~lidar_type', ydlidar.TYPE_TRIANGLE)
-        self.device_type = rospy.get_param('~device_type',ydlidar.YDLIDAR_TYPE_SERIAL)  
-        
-        
-        ############################################################################
-        self.YDLIDAR = ydlidar.CYdLidar() # Initialize the Lidar
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropSerialPort,self.port) # initiliaze the port...
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropSerialBaudrate,self.buad_rate) 
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropScanFrequency,self.scan_frequency)
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropReversion,self.sample_rate)
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropSingleChannel,self.singlechannel)
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropMinAngle,self.max_range)
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropLidarType,self.lidar_type)
-        self.YDLIDAR.setlidaropt(ydlidar.LidarPropDeviceType,self.device_type)
-        
-        rate = rospy.Rate(10)  # 10 Hz
+        self.device_type = rospy.get_param('~device_type', ydlidar.YDLIDAR_TYPE_SERIAL)
 
+        self.YDLIDAR = ydlidar.CYdLidar()  # Initialize the Lidar
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropSerialPort, self.port)  # initialize the port
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropSerialBaudrate, self.baud_rate)
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropScanFrequency, self.scan_frequency)
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropReversion, self.sample_rate)
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropSingleChannel, self.singlechannel)
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropMaxRange, self.max_range)
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropLidarType, self.lidar_type)
+        self.YDLIDAR.setlidaropt(ydlidar.LidarPropDeviceType, self.device_type)
+
+        self.rate = rospy.Rate(10)  # 10 Hz
+
+
+
+    # initilize the lidar....
+    def initialize_lidar(self):
+        lidar = {'min_angle': -math.pi,
+                 'max_angle': math.pi, 
+                 'angle_increment': math.pi / 180.0, 
+                 'min_range': 0.1, 
+                 'max_range':12.0,
+                 'scan_frequency': 10.0,}
+        return lidar
+    
+    
+    
+    def get_lidar_data(self):
+        # Placeholder for LiDAR data retrieval
+        num_measurements = 360  # My laser rotates in an angle of 360 degrees...
+        ranges = [random.uniform(0.1, 16.0) for _ in range(num_measurements)]
+        intensities = [random.uniform(0, 255) for _ in range(num_measurements)]
+        angles = [self.lidar['min_angle'] + i * self.lidar['angle_increment'] for i in range(num_measurements)]
+        return ranges, intensities, angles
+ 
+ 
+ 
+    # Get laser scan data, process it and publish it to ROS(Method that has the main implementation....)
+    def scan_data(self):   
+        ret_ = self.YDLIDAR.initialize()
+        if ret_ :
+            self.YDLIDAR.turnOn()
+            rospy.logerr("start Lidar scanning!")
+        else:
+            rospy.logerr("Failed to start Lidar scanning!")
         
-     
-    # get laser scan data , process it and publish it over to ros...  
-    def scan_data(self,msg):
-      while not rospy.is_shutdown():
-        # Read Lidar Scan data
-        self.laser_scan_data = ydlidar.LaserScan() # get data from the laser scanner...
-        self.scan = LaserScan()
-        # Populate the scan data....
-        data_ = self.YDLIDAR.doProcessSimple(self.laser_scan_data)
-        if data_ :  # if true
-          angle_range = []
-          ran = []
-          laser_intensity = []
-          
-          for points in self.laser_scan_data:
-            angle_range.append(points.angle)
-            
-            
-         
-          
-          
-          
-          # Publish "/scan" topic over to ros....
-          # self.laser_pub.publish(self.scan)
-          
-          
-          
-          
-          
-          
-        
-      
-      
         
         
+        # Get the lidar parameter initilization stored in a varaible.....
+        self.lidar = self.initialize_lidar()
         
         
-        
-        
-        
-        
-        
-        
+        # Loop true.....  
+        while not rospy.is_shutdown():
+            ranges, intensities, angles = self.get_lidar_data()
+            laser_scan_data = ydlidar.LaserScan()  # get data from the laser scanner
+            if self.YDLIDAR.doProcessSimple(laser_scan_data):
+            # self.rate.sleep()
+            # self.YDLIDAR.turnOn()
+                scan = LaserScan()
+                scan.header.stamp = rospy.Time.now()
+                scan.header.frame_id = "laser_frame"
+                scan.angle_min = self.lidar['min_angle']
+                scan.angle_max = self.lidar['max_angle']
+                scan.angle_increment = self.lidar['angle_increment']
+                # scan.time_increment = laser_scan_data.config.time_increment
+                scan.scan_time = scan.scan_time / len(ranges)
+                scan.range_min = self.lidar['min_range']
+                scan.range_max = self.lidar['max_range']
+                scan.ranges = ranges
+                scan.intensities = intensities
+
+                # Publish "/scan" topic to ROS
+                self.laser_pub.publish(scan)
+            self.rate.sleep()
+
